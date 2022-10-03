8fbd9b2f877b4c28f72ff349892e6af998cc934e
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-10-03 20:03:44 +0300

    сделал 2-е как у Ильи

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,45 +1,45 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
-
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
 // import Player from '@vimeo/player';
-// console.log('Player', Player);
 // import throttle from 'lodash.throttle';
+
 // const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const SAVE_TIME = 'videoplayer-current-time';
-// // ****************************************************
-// const onPlay = function (data) {
-//   localStorage.setItem(SAVE_TIME, JSON.stringify(data['seconds']));
-// };
+// const player = new Vimeo.Player(iframe);
 
-// player.on('timeupdate', throttle(onPlay, 1000));
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-// time();
-// function time() {
-//   const currentTime = JSON.parse(localStorage.getItem(SAVE_TIME));
-//   if (currentTime === 0) {
-//     return;
-//   }
-//   player
-//     .setCurrentTime(currentTime)
-//     .then(function (seconds) {})
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           break;
-//         default:
-//           break;
-//       }
-//     });
-// }
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+import Player from '@vimeo/player';
+console.log('Player', Player);
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const SAVE_TIME = 'videoplayer-current-time';
+// ****************************************************
+const onPlay = function (data) {
+  localStorage.setItem(SAVE_TIME, JSON.stringify(data['seconds']));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+time();
+function time() {
+  const currentTime = JSON.parse(localStorage.getItem(SAVE_TIME));
+  if (currentTime === 0) {
+    return;
+  }
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    });
+}
