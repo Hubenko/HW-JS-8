cd05e3dfd3920a7d15fc680117b7ab300a12b8c0
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-10-05 18:27:57 +0300

    2-е сделал

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,9 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
 import Player from '@vimeo/player';
-console.log('Player', Player);
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const SAVE_TIME = 'videoplayer-current-time';
-// ****************************************************
+
 const onPlay = function (data) {
   localStorage.setItem(SAVE_TIME, JSON.stringify(data['seconds']));
 };
